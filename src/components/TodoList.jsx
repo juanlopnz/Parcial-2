ea0708dd7b6ef0e2239c14a5be4388503d2eb326
juanlopnz <jucalora17@gmail.com>
2023-05-17 14:34:21 -0500

    Parcial terminado

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -17,8 +17,8 @@ export const TodoList = () => {
   };
 
   return (
-    <div className="grid grid-cols-2 justify-between w-full h-screen px-8 py-8 bg-gray-200">
-      <div className="flex flex-col items-center">
+    <div className="flex flex-col md:flex-row items-start md:items-center justify-between w-full h-screen px-8 py-8 bg-gray-200">
+      <div className="md:w-3/5 w-full flex flex-col items-center">
         <h1 className="text-4xl font-bold text-gray-700 mb-6">Todo List</h1>
         {post.length ? (
           <ul className="w-full flex flex-wrap justify-center gap-4">
@@ -26,7 +26,7 @@ export const TodoList = () => {
               <li
                 onClick={() => handleClick(index)}
                 key={index}
-                className="w-64 h-64 rounded-lg"
+                className="cursor-pointer w-64 h-64 rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
               >
                 <img
                   src={item.url}
@@ -44,14 +44,13 @@ export const TodoList = () => {
           </div>
         )}
       </div>
-      <div className="">
+      <div className="md:w-2/5 w-full mt-8 md:mt-0 right-0 fixed">
         {selectedPost ? (
-          <div className="w-full rounded-lg justify-center">
-            <h1 className='text-center text-5xl'>Cuadro Selecionado</h1>
+          <div className="w-full rounded-lg overflow-hidden shadow-lg">
             <img
               src={selectedPost.url}
               alt=""
-              className="w-80 h-80 object-cover object-center"
+              className="w-full h-80 object-cover object-center"
             />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">
